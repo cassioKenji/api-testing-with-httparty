419c6ccdc3cdbc72fb4480ea87589cf31c0b6e0d
Cássio Kenji <cassio.oshiro@walmart.com>
2016-08-24 00:11:31 -0300

    add a few more steps to steps.rb regarding health check tests

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,16 +1,19 @@
-require '././beernivore/beernivore_app.rb'
-require 'mock5'
-
-Mock5.mount(@beernivore_api)
-
 Given(/^a Beernivore API up and running$/) do
-  Mock5.mounted_apis.class
+  Mock5.mounted_apis
 end
 
 When(/^I make a get in "([^"]*)"$/) do |route|
-  @response = HTTParty.get("https://beernivore-api.com#{route}")
+  @response = HTTParty.get("https://beernivore.com#{route}")
+end
+
+Then(/^it's headers must contain the value "([^"]*)" within the "([^"]*)"$/) do |value, header|
+  expect(@response.headers[header]).to eql(value)
 end
 
 Then(/^it should return a response with a (\d+) response code$/) do |code|
   expect(@response.code).to eql(code.to_i)
+end
+
+Then(/^it's body must contain the value "([^"]*)" within the "([^"]*)"$/) do |value, att|
+  expect(@response[att]).to eql(value)
 end
