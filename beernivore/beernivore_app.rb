e39396c0e8d93fc53eef12132d8f59b5fc4376bd
Cássio Kenji <cassio.oshiro@walmart.com>
2016-08-25 19:58:36 -0300

    add the db health check to the response

--- beernivore/beernivore_app.rb
+++ beernivore/beernivore_app.rb
@@ -1,11 +1,13 @@
-require 'mock5'
+require "mock5"
 
 @beernivore_api = Mock5.mock("https://beernivore.com") do
-
   get "/health_check" do
-    headers['Content-Type'] = "application/json"
+    headers["Content-Type"] = "application/json"
 
-    return {:healthy => "true", :message => "success"}.to_json
+    return { healthy: "true",
+             message: "success",
+             # for now, the database does not matter for us :)
+             db: { healthy: "true",
+                   message: "success" } }.to_json
   end
-
 end
