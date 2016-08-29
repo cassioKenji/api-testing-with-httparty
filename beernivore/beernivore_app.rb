4022208a5e0619572b8c7ff3c57eced54b88a4fc
Cássio Kenji <cassio.oshiro@walmart.com>
2016-08-29 17:47:31 -0300

    add beer of the day route to the beernivore-api

--- beernivore/beernivore_app.rb
+++ beernivore/beernivore_app.rb
@@ -1,4 +1,5 @@
-require "mock5"
+require 'mock5'
+require 'faker'
 
 @beernivore_api = Mock5.mock("https://beernivore.com") do
   get "/health_check" do
@@ -12,5 +13,19 @@ require "mock5"
                    message: "connected" },
              elasticsearch: { healthy: "true",
                               message: "ok" } }.to_json
+  end
+
+  get "/beer_of_the_day" do
+    headers["Content-Type"] = "application/json"
+
+    return { beer: { name: Faker::Beer.name,
+                     vegan_friendly: "true",
+                     malts: Faker::Beer.malts,
+                     hop: Faker::Beer.hop,
+                     yeast: Faker::Beer.yeast,
+                     alcohol: Faker::Beer.alcohol,
+                     location: Faker::Address.country_code },
+             contact_info: { site: Faker::Internet.url,
+                             email: Faker::Internet.email } }
   end
 end
